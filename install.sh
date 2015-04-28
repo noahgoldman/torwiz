163425243cd5fcc7c3bb7e4fa4c764a86e663555
Jake Martin <jmart.alb@gmail.com>
2015-04-28 16:28:39 -0400

    made fileserver and added links

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #Throw everything in here needed to install stuff
-#including mongo
-#python
-#etc
+
 sudo apt-get install -y python-libtorrent python-libtorrent-dbg libtorrent-rasterbar-dev libtorrent-rasterbar7 python-pip python-dev mongodb
 sudo pip install -r requirements.txt
 
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-python FlaskServer.py
-python run.py
+python FlaskServer/FlaskServer.py&
+python torrents/run.py&
+cd torrents/torrents_files
+python -m SimpleHTTPServer 7999
